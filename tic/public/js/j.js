2055679f60ed834fcc3046bfa6907a072b0cde2c
kckoh <kyungcheolkoh@gmail.com>
2019-11-23 04:06:56 +0000

    tic tac toe adding colors

--- tic/public/js/j.js
+++ tic/public/js/j.js
@@ -3,30 +3,32 @@ var cols =3
 var rows =3
 var grid = new Array(cols)
 var w,h
+var alternate
 
 
 function tic(i,j){
 	this.x = i
 	this.y = j
-	this.color = 0
+    this.color = 0
+    this.isBlack = false
 	this.show = function() {
-        if(this.x ==0 && this.y ==0){
-            fill(0)
+            if (this.isBlack ==false){
+                 fill(255)
+                stroke(0)
+                rect(this.x *w, this.y*h, w ,h)
+                this.isBlack = true
+            }
+            else{
+                 fill(0)
             stroke(0)
-		    rect(this.x *w, this.y*h, w ,h)
-        }
-        else{
-            fill(255)
-            stroke(0)
-            rect(this.x *w, this.y*h, w ,h)
-        }
+            rect(this.x *w, this.y*h, w ,w)
+            this.isBlack = false
+            }
+
+           
 		
 	}
 
-    this.clicked = ()=>{
-        
-        fill(0)
-    }
 
 }
 
@@ -34,10 +36,13 @@ function setup() {
 	
 	createCanvas(300,300);
 	w = width/cols
-	h = height/rows
+    h = height/rows
+    console.log(w,h)
 	for (var i = 0.; i < cols; i++) {
-		grid[i] = new Array(rows)
-	}
+
+        grid[i] = new Array(rows)
+    }
+    
 
 
 	for (var i = 0.; i < cols; i++) {
@@ -59,18 +64,51 @@ function setup() {
 }
 
 function draw(){
-    	for (var i = 0; i <cols; i++) {
-		    for (var j = 0; j < rows; j++) {
-				grid[i][j].show()
-		    }
-	    }
+    	
 
 }
 
 
 function mouseClicked(event) {
-    var clickedX = screenX
+    // First row
+    if(event.screenX < 108 && event.screenY < 170){
+        grid[0][0].show()
+    }
 
+    if( ((event.screenX > 108) && (event.screenX < 208))  && (event.screenY < 170)  ){
+        grid[1][0].show()
+    }
+
+    if( ((event.screenX > 208) && (event.screenX < 308))  && (event.screenY < 170)  ){
+        grid[2][0].show()
+    }
+
+    // second row
+    if(event.screenX < 108 && ((event.screenY > 170) && (event.screenY < 270))){
+        grid[0][1].show()
+    }
     
-  console.log(event.screenX)
+    if(((event.screenX > 108) && (event.screenX < 208)) && ((event.screenY > 170) && (event.screenY < 270))){
+        grid[1][1].show()
+    }
+
+    if(((event.screenX > 208) && (event.screenX < 308)) && ((event.screenY > 170) && (event.screenY < 270))){
+        grid[2][1].show()
+    }
+
+    // third row
+
+    if(event.screenX < 108 && ((event.screenY > 170) && (event.screenY > 270))){
+        grid[0][2].show()
+    }
+
+    if(((event.screenX > 108) && (event.screenX < 208)) && (event.screenY > 270)){
+        grid[1][2].show()
+    }
+
+    if(  ((event.screenX > 208) && (event.screenX < 308))  && (event.screenY > 270)){
+        grid[2][2].show()
+    }
+    
+  console.log(event.screenX, event.screenY)
 }
