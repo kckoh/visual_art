290f04c60d241d6ed80fd14f80c3283251d9a5e1
kckoh <kyungcheolkoh@gmail.com>
2019-11-27 14:57:47 +0000

    finishing the graphic parts

--- bfs/public/js/j.js
+++ bfs/public/js/j.js
@@ -1,19 +1,22 @@
+
+
 function Graph(value){
     this.value = value
     this.edges = []
-    this.parent
+    this.parent = null
     this.discovered = false
     Graph.prototype.addNode = function (n){
         this.edges.push(n)
     }
-
 }
 
+var sel
+var item
 var data
 var kevin
 var movies= new Map()
 var casts = new Map()
-var actor, movie,m
+var cast, movie
 var c = []
 var s = []
 var moviesList
@@ -24,61 +27,62 @@ function preload(){
 }
 
 
+function run(){
+    // var found = bfs(casts.get(sel.value()))
+    // var stack = []
+    // while(found.parent != null){
+    //     stack.push(found)
+    //     found = found.parent
+    // }
+    // stack.push(found)
+
+    // while(stack.length !=0){
+    //     createP(stack.pop().value)
+    // }
+
+    createP(sel.value())
+    
+}
+
 function setup() {
 	//canvas
     noCanvas();
 
+    sel = createSelect();
+    sel.position(250, 10);
+    
+    
     moviesList = data.movies
+
+   
 
     for( var i = 0; i < moviesList.length;i++){
 
         movie = moviesList[i].title
         for( var j = 0; j < moviesList[i].cast.length;j++){
-            // if casts already exist
-            if(casts.has(moviesList[i].cast[j])){
-                var m = new Graph(movie)
-                // adding arrays for all casts except for this cast
-               
-                casts.get(moviesList[i].cast[j]).addNode(m)
-           }
-           else{
-                var cast = new Graph(moviesList[i].cast[j])
-                var m = new Graph(movie)
-                // adding arrays for all casts
-                cast.addNode(m)
-                casts.set(moviesList[i].cast[j],cast)
-                
-           }
+            cast = moviesList[i].cast[j]
+            sel.option(cast);
+            casts.set(cast, new Graph(cast))
+        }
+        movies.set(movie, new Graph(movie) )
+    }
+
+    for( var i = 0; i < moviesList.length;i++){
+        movie = moviesList[i].title
+        for( var j = 0; j < moviesList[i].cast.length;j++){
+            casts.get(moviesList[i].cast[j]).addNode(movies.get(movie))
+            movies.get(movie).addNode(casts.get(moviesList[i].cast[j]))
         }
     }
 
-    console.log(casts.get("Kevin Bacon"))
-
-    // var ex = new Graph(3)
-
-    // var ex1 = new Graph(4)
-    // var ex2 = new Graph(6)
-    // var ex3 = new Graph(7)
-    // var ex4 = new Graph(8)
-
-    // ex.addNode(ex1)
-    // ex1.addNode(ex2)
-    // ex2.addNode(ex3)
-    // ex2.addNode(ex4)
-    // console.log(casts.get("Daniel Stern"))
-    // bfs(ex)
     
-    
-
+    sel.changed(run())
 
     
 }
 
 
-function draw(){
-    	
 
-}
 
 function bfs(start_v){
     var q = []
@@ -90,14 +94,12 @@ function bfs(start_v){
     while (q.length != 0) {
         v = q.shift()
         if (v.value == goal){
-            console.log(v.value)
-            return v.value
+            return v
             
         }
             array1 = v.edges;
             array1.forEach(element => {
                 if(element.discovered == false){
-                    console.log(element)
                     element.discovered = true
                     element.parent = v
                     q.push(element)
@@ -109,28 +111,3 @@ function bfs(start_v){
 }
 
 
-//  for( var i = 0; i < moviesList.length;i++){
-
-//         movie = moviesList[i].title
-//         for( var j = 0; j < moviesList[i].cast.length;j++){
-//             // adding graphs of casts to array
-//             c.push(new Graph(moviesList[i].cast[j]))
-//             // if casts already exist
-//             if(casts.has(moviesList[i].cast[j])){
-//                 // add this new graph of movie title
-//                 s.push(new Graph(movie))
-//                 // adding what was already there
-//                 casts.get(moviesList[i].cast[j]).forEach(element => s.push(element))
-//                 casts.set(moviesList[i].cast[j],s)
-//                 s = []
-//             }
-//             else{
-//                 casts.set(moviesList[i].cast[j],[new Graph(movie)])
-//             }
-//         }
-        
-//         // setting the movie titles with graphs of casts
-//         movies.set(movie, c)
-//         c = []
-        
-//     }
